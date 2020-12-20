0f352faa4cfebb9fbf28ff6f904a164b98737d1e
yfishaq <76264601+yfishaq@users.noreply.github.com>
2020-12-20 11:50:38 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ import aItems from "../items/index.js";
 class Page {
     constructor(){
         this.sName = "Yusra Farrukh";
-        this.sUrlToEmailer = "https://dry-bayou-18746.herokuapp.com/send/";
+        this.sUrlToEmailer = "https://shrouded-beach-31020.herokuapp.com/send/";
         const sBase = document.location.pathname;
         if(sBase[sBase.length - 1] == "/"){
             this.sBase = sBase.substr(0, sBase.length -1);
