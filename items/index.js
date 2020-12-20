da0083bd1a1b8138858f772608770f95ebc7f873
Yusra Farrukh <sfishaq@gmail.com>
2020-12-20 18:45:06 -0500

    Adding Images

--- items/index.js
+++ items/index.js
@@ -1,5 +1,2 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/FavouriteFoodBaskets750x500_xraj9x.png"},
-{"id": "item2", "fname":"item2.md", "specialImage":"/img/FavouriteDay750x500_pdni4p.png"},
-{"id": "item3", "fname":"item3.md", "specialImage":"/img/twiliobot750x500_z7anzp.png"},
-{"id": "item4", "fname":"item4.md", "specialImage":"https://res.cloudinary.com/salesucation-com-inc/image/upload/v1523215138/DroidfootGame750x500_bwphbe.png"},
-{"id": "item5", "fname":"item5.md", "specialImage":"https://res.cloudinary.com/salesucation-com-inc/image/upload/v1523468836/def-t750x500_gv1zbk.png"}];
+export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/3-in-1 Coat.png"},
+{"id": "item2", "fname":"item2.md", "specialImage":"/img/Therapy Box.png"}];
