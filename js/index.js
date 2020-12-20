bf8b275fa714efe71f9a5fd50380c5d8f20ed463
Yusra Farrukh <sfishaq@gmail.com>
2020-12-20 14:52:07 -0500

    change copyright

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ import aItems from "../items/index.js";
 
 class Page {
     constructor(){
-        this.sName = "Yusra Farrukh";
+        this.sName = "Yusra Farruh";
         this.sUrlToEmailer = "https://shrouded-beach-31020.herokuapp.com/send/";
         const sBase = document.location.pathname;
         if(sBase[sBase.length - 1] == "/"){
